aeef3f7600ce1a34ef359c5a2522f061b95424e7
Vince Melia <vince@drifta.co.uk>
2021-03-03 13:04:30 +0000

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        public void Calculate(List<int> values)
+        {
+            Average = 99.9; //ToDo: Calculate this.
+            Total = 9; //ToDo: Calculate this.
+        }
+
+        public double Average { get; private set; }
+        public int Total { get; private set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleList = new List<int> { 1, 2, 4, 8 };
+            var calculator = new Calculator();
+            calculator.Calculate(sampleList);
+            Console.WriteLine($"Average: {calculator.Average}, Total: {calculator.Total}.");
+
+            Console.WriteLine("Please enter some whole numbers separated by commas");
+            var enteredText = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(enteredText))
+            {
+                var splitText = enteredText.Split(',');
+
+                var numbers = new List<int>();
+                foreach (var potentialNumber in splitText)
+                {
+                    if (int.TryParse(potentialNumber, out var i))
+                        numbers.Add(i);
+                }
+
+                if (numbers.Count > 0)
+                {
+                    var total = 0;
+                    foreach (var number in numbers)
+                    {
+                        total += number;
+                    }
+
+                    var average = total / numbers.Count;
+
+                    var numberText = "number";
+                    if (numbers.Count > 1)
+                        numberText += "s";
+
+                    Console.WriteLine($"You entered {numbers.Count} {numberText}.");
+                    Console.WriteLine($"Average: {average}, Total: {total}.");
+                }
+            }
+        }
+    }
+}
